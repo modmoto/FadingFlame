1a44f426b59dd970f8a8bb5f3298149d483c483f
Simon Heiss <simonheiss87@gmail.com>
2021-08-19 16:49:08 +0200

    chieat

--- FadingFlame/Leagues/League.cs
+++ FadingFlame/Leagues/League.cs
@@ -109,16 +109,4 @@ namespace FadingFlame.Leagues
             };
         }
     }
-
-    internal class PointTuple
-    {
-        public PointTuple(int player1, int player2)
-        {
-            Player1 = player1;
-            Player2 = player2;
-        }
-
-        public int Player1 { get; }
-        public int Player2 { get; }
-    }
 }
--- FadingFlame/UserAccounts/UserState.cs
+++ FadingFlame/UserAccounts/UserState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using MongoDB.Bson;
 
 namespace FadingFlame.UserAccounts
@@ -7,15 +8,22 @@ namespace FadingFlame.UserAccounts
     {
         public event EventHandler UserLoggedIn;
 
+        private List<ObjectId> Admins = new()
+        {
+            new("611ce5c80c434f08ea507fa6")
+        };
+
         public virtual void SetUserLoggedIn(ObjectId playerId, string userName)
         {
             UserIsLoggedIn = true;
             LoggedInPlayerId = playerId;
             UserName = userName;
             UserLoggedIn?.Invoke(this, EventArgs.Empty);
+            UserIsAdmin = Admins.Contains(playerId);
         }
 
         public bool UserIsLoggedIn { get; private set; }
+        public bool UserIsAdmin { get; private set; }
         public ObjectId LoggedInPlayerId { get; private set; }
         public string UserName { get; private set; }
     }
