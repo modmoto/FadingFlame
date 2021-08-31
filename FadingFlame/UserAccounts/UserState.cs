04a7222869f752790db73345ae43fb7774e8aaed
modmoto <simonheiss87@gmail.com>
2021-08-31 17:27:51 +0200

    refactor claims to state

--- FadingFlame/UserAccounts/UserState.cs
+++ FadingFlame/UserAccounts/UserState.cs
@@ -1,31 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using MongoDB.Bson;
 
 namespace FadingFlame.UserAccounts
 {
     public class UserState
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserState(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        
         public event EventHandler UserLoggedIn;
 
         private List<string> Admins = new()
         {
-            "AliceSmith@email.com",
             "simonheiss87@gmail.com"
         };
 
-        public virtual void SetUserData(ObjectId playerId, string userName, string email)
+        public virtual void SetUserData(ObjectId playerId, string userName)
         {
-            UserIsLoggedIn = true;
             LoggedInPlayerId = playerId;
             UserName = userName;
-            UserIsAdmin = Admins.Contains(email);
             UserLoggedIn?.Invoke(this, EventArgs.Empty);
         }
 
-        public bool UserIsLoggedIn { get; private set; }
-        public bool UserIsAdmin { get; private set; }
+        public bool UserIsLoggedIn => _httpContextAccessor.HttpContext?.User.HasClaim(c => c.Type == "email") ?? false;
+        public bool UserIsAdmin => Admins.Contains(Email);
         public ObjectId? LoggedInPlayerId { get; private set; }
         public string UserName { get; private set; }
+        public string Email => _httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c => c.Type == "email")?.Value;
+        public string OriginalUserName => _httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c => c.Type == "given_name")?.Value;
     }
 }
