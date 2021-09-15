891e654c8b373373bc7c2e8ae49f5f11720d6af2
modmoto <simonheiss87@gmail.com>
2021-09-15 22:35:18 +0200

    fixed discord bot and figured that tags have to be deleted first

--- FadingFlame/Discord/DiscordBot.cs
+++ FadingFlame/Discord/DiscordBot.cs
@@ -28,7 +28,10 @@ namespace FadingFlame.Discord
                 Token = token,
                 TokenType = TokenType.Bot,
                 AutoReconnect = true,
-                MinimumLogLevel = LogLevel.Debug
+                MinimumLogLevel = LogLevel.Debug,
+                Intents = DiscordIntents.GuildMembers
+                        | DiscordIntents.GuildPresences
+                        | DiscordIntents.Guilds
             };
             
             _client = new DiscordClient(discordConfiguration);
@@ -52,10 +55,11 @@ namespace FadingFlame.Discord
 
             foreach (var clientGuild in _client.Guilds)
             {
-                var leaguesCategory = clientGuild.Value.Channels.FirstOrDefault(c => c.Value.IsCategory && c.Value.Name == "leagues").Value;
+                var guild = clientGuild.Value;
+                var leaguesCategory = guild.Channels.FirstOrDefault(c => c.Value.IsCategory && c.Value.Name == "leagues").Value;
                 if (leaguesCategory == null)
                 {
-                    leaguesCategory = await clientGuild.Value.CreateChannelCategoryAsync("leagues");
+                    leaguesCategory = await guild.CreateChannelCategoryAsync("leagues");
                     await Task.Delay(50);
                 }
 
@@ -63,25 +67,23 @@ namespace FadingFlame.Discord
                 foreach (var league in leagues)
                 {
                     var players = await _playerRepository.LoadForLeague(league.Players.Select(p => p.Id).ToList());
-                    var leagueChannel = clientGuild.Value.Channels.FirstOrDefault(c => c.Value.Type == ChannelType.Text && c.Value.Name == ToLeagueName(league)).Value;
+                    var leagueChannel = guild.Channels.FirstOrDefault(c => c.Value.Type == ChannelType.Text && c.Value.Name == ToLeagueName(league)).Value;
                     if (leagueChannel == null)
                     {
-                        leagueChannel = await clientGuild.Value.CreateTextChannelAsync($"league-{league.DivisionId}-{league.Name}", leaguesCategory);
+                        leagueChannel = await guild.CreateTextChannelAsync($"league-{league.DivisionId}-{league.Name}", leaguesCategory);
                         await Task.Delay(50);
                     }
                     
                     await leagueChannel.ModifyPositionAsync(position);
                     
-                    var role = clientGuild.Value.Roles.FirstOrDefault(r => r.Value.Name == league.DivisionId.ToLower()).Value;
+                    var role = guild.Roles.FirstOrDefault(r => r.Value.Name == league.DivisionId.ToLower()).Value;
                     if (role == null)
                     {
-                        role = await clientGuild.Value.CreateRoleAsync(league.DivisionId.ToLower(), color: LeagueConstants.DiscordColors[position - 1]);
+                        role = await guild.CreateRoleAsync(league.DivisionId.ToLower(), color: LeagueConstants.DiscordColors[position - 1]);
                         await Task.Delay(50);
                     }
 
-                    var owner = clientGuild.Value.Owner;
-                    await GrantRoleForLeague(players, owner, role);
-                    foreach (var discordMember in clientGuild.Value.Members)
+                    foreach (var discordMember in guild.Members)
                     {
                         await GrantRoleForLeague(players, discordMember.Value, role);
                     }
@@ -97,7 +99,7 @@ namespace FadingFlame.Discord
             var playerInLeagues = players.FirstOrDefault(p => p.DiscordTag?.ToLower() == username);
             if (playerInLeagues != null)
             {
-                var oldLeagueRoles = member.Roles.Where(r => LeagueConstants.Ids.Select(l => l.ToLower()).Contains(r.Name));
+                var oldLeagueRoles = member.Roles.Where(r => LeagueConstants.Ids.Select(l => l.ToLower()).Contains(r.Name)).ToList();
                 foreach (var oldLeagueRole in oldLeagueRoles)
                 {
                     await member.RevokeRoleAsync(oldLeagueRole);
