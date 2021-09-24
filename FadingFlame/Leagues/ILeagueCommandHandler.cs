05e638e8892c859af4f3a2b64bd64a866e6ef35b
modmoto <simonheiss87@gmail.com>
2021-09-24 09:39:28 +0200

    google sheet version

--- FadingFlame/Leagues/ILeagueCommandHandler.cs
+++ FadingFlame/Leagues/ILeagueCommandHandler.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using FadingFlame.Admin;
 using FadingFlame.Players;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
 using Microsoft.AspNetCore.Hosting;
 
 namespace FadingFlame.Leagues
@@ -42,22 +45,23 @@ namespace FadingFlame.Leagues
             var players = await _playerRepository.LoadAll();
             await _playerRepository.DeleteAllWithMail(_notfoundMail);
             var newLeagues = new List<League>();
-
-            var allLines = await File.ReadAllLinesAsync($"{_hostEnvironment.WebRootPath}/leagues.csv");
             var notFoundPlayers = new List<string>();
-            var commaDelimitedValues = allLines.Select(l => l.Split(",")).ToList();
+
+            var serviceValues = GetSheetsService().Spreadsheets.Values;
+            
             for (int leagueIndex = 0; leagueIndex < 19; leagueIndex++)
             {
+                var stringsOfExcel = await ReadAsync(serviceValues, leagueIndex);
                 var league = League.Create(currentSeason.SeasonId, currentSeason.StartDate, LeagueConstants.Ids[leagueIndex], LeagueConstants.Names[leagueIndex]);
                 for (int playerIndex = 0; playerIndex < 6; playerIndex++)
                 {
                     var index = playerIndex + 1 * playerIndex;
-                    var discordTag = commaDelimitedValues[index][leagueIndex];
-                    var name = commaDelimitedValues[index + 1][leagueIndex];
+                    var discordTag = stringsOfExcel[index];
+                    var name = stringsOfExcel[index + 1];
 
                     var foundPlayer = players.FirstOrDefault(p => 
                         p.DiscordTag?.ToLower() == discordTag.ToLower()
-                        || p.DisplayName.ToLower() == name.ToLower());
+                        || p.DisplayName.ToLower() == name?.ToLower());
 
                     if (foundPlayer != null)
                     {
@@ -65,7 +69,7 @@ namespace FadingFlame.Leagues
                     }
                     else
                     {
-                        var player = Player.Create($"missing_{discordTag}", _notfoundMail);
+                        var player = Player.Create($"!MISSING_{discordTag}", _notfoundMail);
                         notFoundPlayers.Add($"{discordTag},{name}");
                         await _playerRepository.Insert(player);
                         league.AddPlayer(player);
@@ -79,5 +83,39 @@ namespace FadingFlame.Leagues
             
             await _leagueRepository.Insert(newLeagues);
         }
+        
+        private static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private const string SpreadsheetId = "1WY2I-Nl0n2HgWp2MMjnEpRQ3DmbErQFsNZvR8DMgJeM";
+        private const string GoogleCredentialsFileName = "fading-flame-babbfd3eb7b2.json";
+       
+        private SheetsService GetSheetsService()
+        {            
+            using (var stream = new FileStream($"{_hostEnvironment.WebRootPath}/{GoogleCredentialsFileName}", FileMode.Open, FileAccess.Read))
+            {
+                var serviceInitializer = new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = GoogleCredential.FromStream(stream).CreateScoped(Scopes)
+                };
+                return new SheetsService(serviceInitializer);
+            }
+        }
+        
+        private async Task<List<string>> ReadAsync(SpreadsheetsResource.ValuesResource valuesResource, int index)
+        {
+            var response = await valuesResource.Get(SpreadsheetId, $"{_chars[index]}2:{_chars[index]}14").ExecuteAsync();
+            var values = response.Values;
+            var strings = values.Select(v => v.FirstOrDefault()?.ToString()).ToList();
+            if (strings.Count == 11)
+            {
+                strings.Add(null);
+            }
+            return strings.ToList();
+        }
+
+        private List<string> _chars = new()
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "R", "P", "R", "S", "T", "U",
+            "V", "W", "X"
+        };
     }
 }
