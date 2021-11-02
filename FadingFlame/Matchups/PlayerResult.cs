783a23916200bf84f4077d2026d24fbd961025d2
simon <simonheiss87@gmail.com>
2021-11-02 18:28:27 +0100

    own files

---
+++ FadingFlame/Matchups/PlayerResult.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+
+namespace FadingFlame.Matchups
+{
+    public class PlayerResult
+    {
+        public ObjectId Id { get; set; }
+
+        /// <summary>
+        /// the points you get by killing enemies
+        /// </summary>
+        public int VictoryPoints { get; set; }
+
+        /// <summary>
+        /// the points you get for secondary objective and victory points difference
+        /// </summary>
+        public int BattlePoints { get; set; }
+
+        public static PlayerResult Create(ObjectId playerId,
+            int battlePoints,
+            int winPoints)
+        {
+            return new()
+            {
+                VictoryPoints = battlePoints,
+                BattlePoints = winPoints,
+                Id = playerId
+            };
+        }
+
+        public static PlayerResult ZeroToZero(ObjectId playerId)
+        {
+            return new ()
+            {
+                Id = playerId
+            };
+        }
+    }
+}
