f37003749975f8c9a827b66161d02e027972297d
simon <simonheiss87@gmail.com>
2021-11-23 18:38:22 +0100

    thing

--- FadingFlame/GlobalLadder/PlayerRankingReadModel.cs
+++ FadingFlame/GlobalLadder/PlayerRankingReadModel.cs
@@ -13,6 +13,7 @@ namespace FadingFlame.GlobalLadder
         public ObjectId Id { get; set; }
         public string Name { get; set; }
         public int Wins { get; set; }
+        public double WinRate { get; set; }
         public int Losses { get; set; }
         public int Draws { get; set; }
         public int Mmr { get; set; }
@@ -24,11 +25,14 @@ namespace FadingFlame.GlobalLadder
         {
             var wins = playersMatches.Count(m => m.Winner == player.Id);
             var losses = playersMatches.Count(m => m.Winner != player.Id && !m.IsDraw);
+            var winRate = losses + wins != 0 ? wins / (double) (wins + losses) : 0;
+            
             return new PlayerRankingReadModel
             {
                 Draws = playersMatches.Count(m => m.IsDraw),
                 Wins = wins,
                 Mmr = (int) player.Mmr.Rating,
+                WinRate = winRate * 100,
                 Losses = losses,
                 Id = player.Id,
                 Name = player.DisplayName
