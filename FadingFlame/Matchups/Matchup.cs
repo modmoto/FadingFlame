062856365f7766339f885978f41bfe19cee5937d
simon <simonheiss87@gmail.com>
2021-11-23 12:54:40 +0100

    added stuff

--- FadingFlame/Matchups/Matchup.cs
+++ FadingFlame/Matchups/Matchup.cs
@@ -21,8 +21,8 @@ namespace FadingFlame.Matchups
             get
             {
                 if (IsChallenge)
-                    return !string.IsNullOrEmpty(ChallengePlayer1List?.List) &&
-                           !string.IsNullOrEmpty(ChallengePlayer2List?.List);
+                    return !string.IsNullOrEmpty(ChallengePlayer1List?.List) && !string.IsNullOrEmpty(ChallengePlayer2List?.List) 
+                        || Result?.Player1List != null && Result?.Player2List != null;
                 return !string.IsNullOrEmpty(Player1List) && !string.IsNullOrEmpty(Player2List);
             }
         }
