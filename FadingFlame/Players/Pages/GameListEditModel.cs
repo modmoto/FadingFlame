db986085b8154b91f63d0cad44bb0198cad90190
Simon Heiss <simonheiss87@gmail.com>
2022-01-08 12:49:27 +0100

    layoffs and challenges now force factions

--- FadingFlame/Players/Pages/GameListEditModel.cs
+++ FadingFlame/Players/Pages/GameListEditModel.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using FadingFlame.Lists;
 
 namespace FadingFlame.Players.Pages
 {
     public class GameListEditModel
     {
+        [Range(1, 20)]
         public Faction Faction { get; set; }
         public string List { get; set; }
         public string ProposedListChange { get; set; }
